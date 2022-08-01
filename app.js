1666cd1f902c306b0f28f5b21fd891853d30bce4
TIMOTHY KUMAR <timothykumar753@gmail.com>
2022-08-01 17:28:10 +0530

    first commit

--- app.js
+++ app.js
@@ -121,11 +121,6 @@ app.get("/:customListName", function (req, res) {
   });
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-
-app.listen(port, function () {
+app.listen(3000, function () {
   console.log("Server started succesfully");
 });
